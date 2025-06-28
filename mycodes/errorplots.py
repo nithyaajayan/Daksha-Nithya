5a8f663c83f01725f35f0ae7beaf61797acf65a1
nithya <nithyaajayan3@gmail.com>
2025-06-29 04:14:34 +0530

    argparse code for distribution plots

--- mycodes/errorplots.py
+++ mycodes/errorplots.py
@@ -7,31 +7,22 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from localisation.locfunction import plotdistribution
 
-def main(error_file):
-    errors = np.load(error_file)
-    
-    all_errors = []
-    for key in errors.files:
-        all_errors.append(errors[key])
-    
-    all_errors = np.concatenate(all_errors, axis=0)
-
-    chi2raerror = all_errors[:,0]
-    chi2decerror = all_errors[:,1]
-    chi2fluxerror = all_errors[:,2]
-    vecraerror = all_errors[:,3]
-    vecdecerror = all_errors[:,4]
-    vecfluxerror = all_errors[:,5]
-
-    bins = np.sqrt(all_errors.shape)
-    
-    plotdistribution(chi2raerror,vecraerror,'Chi2 RA Error',"Vector RA Error",0,"RA Error Distribution", "Error",bins)
-    plotdistribution(chi2decerror,vecdecerror,'Chi2 Dec Error',"Vector Dec Error",0,"Dec Error Distribution", "Error",bins)
-    plotdistribution(chi2fluxerror,vecfluxerror,'Chi2 Flux Error',"Vector Flux Error",0,"Flux Error Distribution", "Error",bins)
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--error_file", type=str, default="localisation_errors.npz")
+    parser.add_argument("--data1_file",type=str,required=True,help=".npy file to dataset1")
+    parser.add_argument("--data2_file",type=str,required=True,help=".npy file to dataset2")
+    parser.add_argument("--true_value",type=float,required=True)
+    parser.add_argument("--data1_label",type=str,required=True)
+    parser.add_argument("--data2_label",type=str,required=True)
+    parser.add_argument("--title",type=str,required=True)
+    parser.add_argument("--xlabel",type=str,required=True)
+    parser.add_argument("--bins",type=int)
     args = parser.parse_args()
-    main(args.error_file)
+    
+    data1 = np.load(args.data1_file)
+    data2 = np.load(args.data2_file)
+    
+    bins = args.bins if args.bins else int(np.sqrt(data1.shape[0]))
+    
+    plotdistribution(data1,data2,args.true_value,args.data1_label,args.data2_label,
+                     args.title, args.xlabel,bins)
