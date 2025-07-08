535b5ff8f87412107e0cf4e1804d38b99bc717b0
nithya <nithyaajayan3@gmail.com>
2025-07-09 03:04:00 +0530

    taking difference from absolute value after fitting

--- mycodes/mollweideerrorplot.py
+++ mycodes/mollweideerrorplot.py
@@ -1,14 +1,11 @@
 import healpy as hp
 import numpy as np
 from collections import defaultdict
-from scipy.optimize import curve_fit
-from scipy.integrate import simpson
-import healpy as hp
 
 NSIDE = 16
 NPIX = hp.nside2npix(NSIDE)
 
-data=np.load("data/test/locmul_ndir_00100_ninj_1000_flu_1e-06_alpha_-1.00_beta_-1.50_Ep_1000.00_NSIDE_128_faces_05.npz",allow_pickle=True)
+data=np.load("data/flu_1e-6/locmul_ndir_10000_ninj_1000_flu_1e-06_alpha_-1.08_beta_-2.14_Ep_196.00_NSIDE_128_faces_08.npz",allow_pickle=True)
 phi_rad = np.radians(data['true_ra'])
 theta_rad =np.radians(90 - data['true_dec'])
 results = data['results']
@@ -18,6 +15,15 @@ mu_phi_chi2 = defaultdict(list)
 sigma_phi_chi2 = defaultdict(list)
 mu_phi_vec = defaultdict(list)
 sigma_phi_vec = defaultdict(list)
+mu_theta_chi2 = defaultdict(list)
+sigma_theta_chi2 = defaultdict(list)
+mu_theta_vec = defaultdict(list)
+sigma_theta_vec = defaultdict(list)
+
+error = defaultdict(list)
+
+from scipy.optimize import curve_fit
+from scipy.integrate import simpson
 
 def gaussian(x, mu, sigma):
     return (1/(sigma*np.sqrt(2*np.pi))) * np.exp(-0.5 * ((x - mu) / sigma)**2)
@@ -29,9 +35,6 @@ def plotdistribution(data, param):
         bins_cen = (bins[:-1] + bins[1:]) / 2
     if (param=="theta"):
         bins = np.arange(-5.5, 5.6, 0.5)
-        bins_cen = (bins[:-1] + bins[1:]) / 2
-    if (param=="fluence"):
-        bins = np.arange(-150.5, 150.6, 5)
         bins_cen = (bins[:-1] + bins[1:]) / 2
 
     data = data[~np.isnan(data)]
@@ -52,48 +55,69 @@ def plotdistribution(data, param):
 
     return popt[0],popt[1]
 
-
 for i in range(len(results)):
     pixel = pix_id[i]
-    injected = results[i,:,:]
+    injected = results[i,:,:]       #1000 x 6
     true_phi = np.degrees(phi_rad[i])
+    true_theta = np.degrees(theta_rad[i])
 
     isnan=0
     for j in range(6):
         if (~np.isnan(injected[:,j])).sum()/injected[:,j].size > 0.5:
             isnan+=1
         
-    if isnan!=6:
+    if isnan<=3:
         continue
 
-    diff_phi_chi2 = injected[:,0] - true_phi
-    diff_phi_vec = injected[:,3] - true_phi
+    mu_c_ph, sig_c_ph = plotdistribution(injected[:,0],param="phi")
+    mu_v_ph,sig_v_ph = plotdistribution(injected[:,3],param="phi")
+    mu_c_th, sig_c_th = plotdistribution(injected[:,1],param="theta")
+    mu_v_th,sig_v_th = plotdistribution(injected[:,4],param="theta")
 
-    mu_c, sig_c = plotdistribution(diff_phi_chi2,param="phi")
-    mu_v,sig_v = plotdistribution(diff_phi_vec,param="phi")
-
-    mu_phi_chi2[pixel].append(mu_c)
-    sigma_phi_chi2[pixel].append(sig_c)
-    mu_phi_vec[pixel].append(mu_v)
-    sigma_phi_vec[pixel].append(sig_v)
+    diff_phi_chi2 = mu_c_ph - true_phi
+    diff_phi_vec = mu_v_ph - true_phi
+    diff_theta_chi2 = mu_c_th - true_theta
+    diff_theta_vec = mu_v_th - true_theta
 
 
-mu_map_chi2 = np.full(NPIX, np.nan)
-sigma_map_chi2 = np.full(NPIX, np.nan)
-mu_map_vec = np.full(NPIX,np.nan)
-sigma_map_vec = np.full(NPIX,np.nan)
+    mu_phi_chi2[pixel].append(diff_phi_chi2)
+    sigma_phi_chi2[pixel].append(sig_c_ph)
+    mu_phi_vec[pixel].append(diff_phi_vec)
+    sigma_phi_vec[pixel].append(sig_v_ph)
+    mu_theta_chi2[pixel].append(diff_theta_chi2)
+    sigma_theta_chi2[pixel].append(sig_c_th)
+    mu_theta_vec[pixel].append(diff_theta_vec)
+    sigma_theta_vec[pixel].append(sig_v_th)
+
+
+mu_map_chi2_ph = np.full(NPIX, np.nan)
+sigma_map_chi2_ph = np.full(NPIX, np.nan)
+mu_map_vec_ph = np.full(NPIX,np.nan)
+sigma_map_vec_ph = np.full(NPIX,np.nan)
+mu_map_chi2_th = np.full(NPIX, np.nan)
+sigma_map_chi2_th = np.full(NPIX, np.nan)
+mu_map_vec_th = np.full(NPIX,np.nan)
+sigma_map_vec_th = np.full(NPIX,np.nan)
 
 for pix in mu_phi_chi2:
-    mu_map_chi2[pix] = np.abs(np.mean(mu_phi_chi2[pix]))
-    sigma_map_chi2[pix] = np.abs(np.mean(sigma_phi_chi2[pix]))
-    mu_map_vec[pix] = np.abs(np.mean(mu_phi_vec[pix]))
-    sigma_map_vec[pix] = np.abs(np.mean(sigma_phi_vec[pix]))
 
-hp.mollview(mu_map_chi2, title="$\mu$ $\Delta$ $\phi$ Chi2", unit="deg")
-hp.mollview(sigma_map_chi2, title="$\sigma$ $\Delta$ $\phi$ Chi2", unit="deg")
-hp.mollview(mu_map_vec, title="$\mu$ $\Delta$ $\phi$ Vector", unit="deg")
-hp.mollview(sigma_map_vec, title="$\sigma$ $\Delta$ $\phi$ Vector", unit="deg")
+    mu_map_chi2_ph[pix] = np.abs(np.mean(mu_phi_chi2[pix]))
+    sigma_map_chi2_ph[pix] = np.abs(np.mean(sigma_phi_chi2[pix]))
+    mu_map_vec_ph[pix] = np.abs(np.mean(mu_phi_vec[pix]))
+    sigma_map_vec_ph[pix] = np.abs(np.mean(sigma_phi_vec[pix]))
+    mu_map_chi2_th[pix] = np.abs(np.mean(mu_theta_chi2[pix]))
+    sigma_map_chi2_th[pix] = np.abs(np.mean(sigma_theta_chi2[pix]))
+    mu_map_vec_th[pix] = np.abs(np.mean(mu_theta_vec[pix]))
+    sigma_map_vec_th[pix] = np.abs(np.mean(sigma_theta_vec[pix]))
 
+import healpy as hp
 
-
+hp.mollview(mu_map_chi2_ph, title="$\mu$ $\Delta$ $\phi$ Chi2", unit="deg")
+hp.mollview(sigma_map_chi2_ph, title="$\sigma$ $\Delta$ $\phi$ Chi2", unit="deg")
+hp.mollview(mu_map_vec_ph, title="$\mu$ $\Delta$ $\phi$ Vector", unit="deg")
+hp.mollview(sigma_map_vec_ph, title="$\sigma$ $\Delta$ $\phi$ Vector", unit="deg")
+hp.mollview(mu_map_chi2_th, title="$\mu$ $\Delta$ $\\theta$ Chi2", unit="deg")
+hp.mollview(sigma_map_chi2_th, title="$\sigma$ $\Delta$ $\\theta$ Chi2", unit="deg")
+hp.mollview(mu_map_vec_th, title="$\mu$ $\Delta$ $\\theta$ Vector", unit="deg")
+#hp.mollview(sigma_map_vec_th, title="$\sigma$ $\Delta$ $\\theta$ Vector", unit="deg")
 
